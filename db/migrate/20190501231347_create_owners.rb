4669538976a804f7525f226602221d5812f31283
illy_kwon <illy.kwon.1221@gmail.com>
2019-05-02 08:15:54 +0900

    Migrate Owner model

---
+++ db/migrate/20190501231347_create_owners.rb
@@ -0,0 +1,12 @@
+class CreateOwners < ActiveRecord::Migration[5.2]
+  def change
+    create_table :owners do |t|
+      t.string :name, null: false
+      t.string :phone_number, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_224901) do
+ActiveRecord::Schema.define(version: 2019_05_01_231347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "phone_number", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "purchases", force: :cascade do |t|
     t.uuid "user_id"
